67aa79dbb2f8a4a4ca3b113be195d7157939d222
vincent.hoehn <78547173+vincenthoehn@users.noreply.github.com>
2024-10-25 08:35:23 +0200

    Try to fix runtime error.

--- js/ckeditor5_plugins/textselection/src/textselection/textselection.js
+++ js/ckeditor5_plugins/textselection/src/textselection/textselection.js
@@ -30,7 +30,7 @@ export default class Textselection extends Plugin {
                 this.listenTo(sourceEditing, 'change:isSourceEditingMode', (evt, propertyName, isSourceEditingMode) => {
                     view.isEnabled = !isSourceEditingMode;  // Disable in SourceEditing mode
                     if (!isSourceEditingMode) {
-                        // Only attempt scroll when back in WYSIWYG
+                        // Only attempt scroll when back in WYSIWYG mode
                         this.scrollToCursorPosition();
                     }
                 });
@@ -41,14 +41,19 @@ export default class Textselection extends Plugin {
     }
 
     scrollToCursorPosition() {
-        const domConverter = this.editor.editing.view.domConverter;
-        const viewSelection = this.editor.editing.view.document.selection;
-        const domRange = domConverter.viewRangeToDom(viewSelection.getFirstRange());
+        // Check if the editor is in WYSIWYG mode before proceeding
+        const isInWYSIWYG = !this.editor.plugins.get('SourceEditing').isSourceEditingMode;
 
-        if (domRange) {
-            const scrollTarget = domRange.startContainer;
-            if (scrollTarget && scrollTarget.nodeType === Node.ELEMENT_NODE) {
-                scrollTarget.scrollIntoView({ behavior: 'smooth', block: 'center' });
+        if (isInWYSIWYG) {
+            const domConverter = this.editor.editing.view.domConverter;
+            const viewSelection = this.editor.editing.view.document.selection;
+            const domRange = domConverter.viewRangeToDom(viewSelection.getFirstRange());
+
+            if (domRange) {
+                const scrollTarget = domRange.startContainer;
+                if (scrollTarget && scrollTarget.nodeType === Node.ELEMENT_NODE) {
+                    scrollTarget.scrollIntoView({ behavior: 'smooth', block: 'center' });
+                }
             }
         }
     }
