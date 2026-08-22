e63f77e12e6b2e84a20629e9b8a00d736b05a7ea
vincent.hoehn <vincent.hoehn99@web.de>
2026-08-22 12:55:46 +0200

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -51,18 +51,26 @@ getDirectories('./js/ckeditor5_plugins').forEach((dir) => {
         core: path.join(__dirname, 'core'),
       },
     },
+    // NEU: Ersetzt die alte DllReferencePlugin-Strategie. Seit der Umstellung
+    // von Drupal Core auf UMD-Builds liefert das ckeditor5-npm-Paket keine
+    // ckeditor5-dll.manifest.json mehr aus. Stattdessen werden alle
+    // CKEditor5-Importe zur Laufzeit aus dem globalen `window.CKEDITOR`-Objekt
+    // aufgelöst, das der von Drupal Core geladene ckeditor5.umd.js-Bundle
+    // bereitstellt.
+    externals: [
+      function ({ request }, callback) {
+        if (request === 'ckeditor5' || request.startsWith('ckeditor5/')) {
+          return callback(null, 'CKEDITOR');
+        }
+        if (request.startsWith('@ckeditor/')) {
+          return callback(null, 'CKEDITOR');
+        }
+        callback();
+      },
+    ],
     plugins: [
       new webpack.optimize.LimitChunkCountPlugin({
         maxChunks: 1,
-      }),
-      // It is possible to require the ckeditor5-dll.manifest.json used in
-      // core/node_modules rather than having to install CKEditor 5 here.
-      // However, that requires knowing the location of that file relative to
-      // where your module code is located.
-      new webpack.DllReferencePlugin({
-        manifest: require('ckeditor5/build/ckeditor5-dll.manifest.json'), // eslint-disable-line global-require, import/no-unresolved
-        scope: 'ckeditor5/src',
-        name: 'CKEditor5.dll',
       }),
     ],
     module: {
